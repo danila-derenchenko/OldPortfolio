1d71c1d29af324d0edf10ebc0f175a2acc9fb390
danila-derenchenko <danila.derenchenko@gmail.com>
2023-06-02 22:53:33 +0300

    update contact block

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
-const myPochta = "danila.derenchenko@yandex.ru";
-const copyButton = document.querySelector(".boxPochta");
-copyButton.addEventListener("click", () => {
+const myPochta = "danila.derenchenko@yandex.ru"
+const myPhone = "+79297066917"
+
+const copyEmail = document.querySelector(".boxPochta")
+const copyPhone = document.querySelector(".phoneBox")
+
+copyEmail.addEventListener("click", () => {
     navigator.clipboard.writeText(myPochta)
     .then (() => {
         alert("Email скопирован в буфер обмена")
+    })
+    .catch (() => {
+        console.error("Ошибка копирования в буфер обмена")
+    })
+})
+
+copyPhone.addEventListener("click", () => {
+    navigator.clipboard.writeText(myPhone)
+    .then (() => {
+        alert("Номер телефона скопирован в буфер обмена")
     })
     .catch (() => {
         console.error("Ошибка копирования в буфер обмена")
